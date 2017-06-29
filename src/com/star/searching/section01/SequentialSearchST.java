820d9915e5115188df99647b94a762e330be3b3a
crystalDf <chendong333@gmail.com>
2017-06-29 21:15:21 +0800

    Chapter 03

--- src/com/star/searching/section01/SequentialSearchST.java
+++ src/com/star/searching/section01/SequentialSearchST.java
@@ -1,7 +1,12 @@
 package com.star.searching.section01;
 
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.StdOut;
+
 public class SequentialSearchST<Key, Value> {
 
+    private int n;
     private Node first;
 
     private class Node {
@@ -41,19 +46,70 @@ public class SequentialSearchST<Key, Value> {
         }
 
         first = new Node(key, value, first);
+
+        n++;
     }
 
     public void delete(Key key) {
 
+        first = delete(first, key);
+    }
+
+    private Node delete(Node x, Key key) {
+
+        if (key.equals(x.key)) {
+            n--;
+
+            return x.next;
+        }
+
+        x.next = delete(x.next, key);
+
+        return x;
+    }
+
+    public boolean contains(Key key) {
+
+        return get(key) != null;
+    }
+
+    public boolean isEmpty() {
+
+        return n == 0;
     }
 
     public int size() {
 
-        return 0;
+        return n;
     }
 
     public Iterable<Key> keys() {
 
-        return null;
+        Queue<Key> queue = new Queue<>();
+
+        for (Node x = first; x != null; x = x.next) {
+
+            queue.enqueue(x.key);
+        }
+
+        return queue;
+    }
+
+    public static void main(String[] args) {
+
+        SequentialSearchST<String, Integer> sequentialSearchST =
+                new SequentialSearchST<>();
+
+        String[] array = new In(args[0]).readAllStrings();
+
+        for (int i = 0; i < array.length; i++) {
+
+            sequentialSearchST.put(array[i], i);
+        }
+
+        for (String string : sequentialSearchST.keys()) {
+
+            StdOut.println(string + " " + sequentialSearchST.get(string));
+        }
     }
 }
