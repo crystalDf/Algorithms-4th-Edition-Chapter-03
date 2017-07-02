c21b1f52102d75dab548373a1813667bf585d8e8
crystalDf <chendong333@gmail.com>
2017-07-02 21:18:23 +0800

    Chapter 03

--- src/com/star/searching/section01/BinarySearchST.java
+++ src/com/star/searching/section01/BinarySearchST.java
@@ -2,6 +2,8 @@ package com.star.searching.section01;
 
 public class BinarySearchST<Key extends Comparable<Key>, Value> {
 
-
+    private Key[] keys;
+    private Value[] values;
+    private int n;
 
 }
