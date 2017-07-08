a65ac14da66bb272bbea927762d43fbb1d5e824f
crystalDf <chendong333@gmail.com>
2017-07-08 20:39:19 +0800

    Chapter 03

--- src/com/star/searching/section02/BST.java
+++ src/com/star/searching/section02/BST.java
@@ -2,4 +2,44 @@ package com.star.searching.section02;
 
 public class BST<Key extends Comparable<Key>, Value> {
 
+    private Node root;
+
+    private class Node {
+
+        private Key key;
+        private Value value;
+        private Node left;
+        private Node right;
+        private int n;
+
+        public Node(Key key, Value value, int n) {
+            this.key = key;
+            this.value = value;
+            this.n = n;
+        }
+    }
+
+    public int size() {
+
+        return size(root);
+    }
+
+    private int size(Node x) {
+
+        if (x == null) {
+            return 0;
+        } else {
+            return x.n;
+        }
+    }
+
+    public Value get(Key key) {
+
+        return null;
+
+    }
+
+    public void put(Key key, Value value) {
+
+    }
 }
