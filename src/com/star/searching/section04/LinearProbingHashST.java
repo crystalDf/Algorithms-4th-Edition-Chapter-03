a3ced13fa57581a0bee8338c5260c33f90b2c0ca
crystalDf <chendong333@gmail.com>
2017-07-22 15:48:50 +0800

    Chapter 03

--- src/com/star/searching/section04/LinearProbingHashST.java
+++ src/com/star/searching/section04/LinearProbingHashST.java
@@ -1,4 +1,10 @@
 package com.star.searching.section04;
 
 public class LinearProbingHashST<Key, Value> {
+
+    private int n;
+    private int m = 16;
+
+    private Key[] keys;
+    private Value[] values;
 }
