1b29a413458773634b90bd212cf03fba1b46a351
crystalDf <chendong333@gmail.com>
2017-08-04 23:26:21 +0800

    Chapter 03

--- src/com/star/searching/section05/DeDup.java
+++ src/com/star/searching/section05/DeDup.java
@@ -1,8 +1,11 @@
 package com.star.searching.section05;
 
+import java.util.HashSet;
+
 public class DeDup {
 
     public static void main(String[] args) {
 
+        HashSet<String> hashSet;
     }
 }
