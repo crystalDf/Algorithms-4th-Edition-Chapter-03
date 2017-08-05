58c1c056482af2e47a02f9d7b4634dad0ca34fa0
crystalDf <chendong333@gmail.com>
2017-08-05 10:05:07 +0800

    Chapter 03

--- src/com/star/searching/section05/DeDup.java
+++ src/com/star/searching/section05/DeDup.java
@@ -1,11 +1,24 @@
 package com.star.searching.section05;
 
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
 import java.util.HashSet;
 
 public class DeDup {
 
     public static void main(String[] args) {
 
-        HashSet<String> hashSet;
+        HashSet<String> hashSet = new HashSet<>();
+
+        String[] array = new In(args[0]).readAllStrings();
+
+        for (int i = 0; i < array.length; i++) {
+
+            if (!hashSet.contains(array[i])) {
+                hashSet.add(array[i]);
+                StdOut.println(array[i]);
+            }
+        }
     }
 }
